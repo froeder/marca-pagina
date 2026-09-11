0444e626fdc251e7a04d2fd4ff342582dfb99507
Jhonatan Froeder <froeder3@gmail.com>
2026-09-11 17:34:10 -0300

    chore: bump version to 1.0.7

--- src/version.ts
+++ src/version.ts
@@ -1,4 +1,4 @@
 // Arquivo gerado automaticamente a cada build. Não edite manualmente.
-export const APP_VERSION = '1.0.6';
-export const BUILD_DATE = '11/09/2026 às 17:29';
-export const BUILD_TIMESTAMP = 1789158550215;
+export const APP_VERSION = '1.0.7';
+export const BUILD_DATE = '11/09/2026 às 17:30';
+export const BUILD_TIMESTAMP = 1789158618911;
