7068a3977f483f3353c5d7e2fd898fb108725e6f
Jhonatan Froeder <froeder3@gmail.com>
2026-09-11 16:21:27 -0300

    feat: update version to 1.0.5 and adjust build date and timestamp

--- src/version.ts
+++ src/version.ts
@@ -1,4 +1,4 @@
 // Arquivo gerado automaticamente a cada build. Não edite manualmente.
-export const APP_VERSION = '1.0.4';
-export const BUILD_DATE = '11/09/2026 às 16:16';
-export const BUILD_TIMESTAMP = 1789154216577;
+export const APP_VERSION = '1.0.5';
+export const BUILD_DATE = '11/09/2026 às 16:19';
+export const BUILD_TIMESTAMP = 1789154380886;
