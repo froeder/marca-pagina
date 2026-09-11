6501146f6d95c7d57dd127b3a52b5939dccbd54d
Jhonatan Froeder <froeder3@gmail.com>
2026-09-11 20:23:29 -0300

    feat: update version to 1.0.12 and regenerate build metadata

--- src/version.ts
+++ src/version.ts
@@ -1,4 +1,4 @@
 // Arquivo gerado automaticamente a cada build. Não edite manualmente.
-export const APP_VERSION = '1.0.11';
-export const BUILD_DATE = '11/09/2026 às 20:06';
-export const BUILD_TIMESTAMP = 1789167968990;
+export const APP_VERSION = '1.0.12';
+export const BUILD_DATE = '11/09/2026 às 20:22';
+export const BUILD_TIMESTAMP = 1789168946390;
